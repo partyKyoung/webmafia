d937ca44c85b56bb9c7227ead83289f12389a921
kyoungah <kyoungah@kyoungah.com>
2017-11-10 23:25:54 +0900

    moment 추가

--- src/container/home/Home.js
+++ src/container/home/Home.js
@@ -1,9 +1,18 @@
 import React, { Component } from 'react';
+import moment from 'moment';
 
 class Home extends Component {
+  constructor (props) {
+    super(props);
+
+    this.state = {};
+  }
+
+
   render () {
     return (
       <div className="home">
+        용식이 전역까지 남은 시간: 
         <img src="/img/package.png" alt="package"/>
       </div>
     );
