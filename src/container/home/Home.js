3b5ef09b701d07e9216596115c64b69ddc9e7a5f
kyoungah <kyoungah@kyoungah.com>
2017-11-11 19:59:51 +0900

    회원가입 폼 작성

--- src/container/home/Home.js
+++ src/container/home/Home.js
@@ -5,7 +5,7 @@ import Ssiam from './Ssiam';
 class Home extends Component {
   render () {
     return (
-      <div className="home">
+      <div className="container">
         <Ssiam />
         <img src="/img/package.png" alt="package"/>
       </div>
--- src/container/user/SignUp.js
+++ src/container/user/SignUp.js
@@ -1,10 +1,33 @@
 import React, { Component } from 'react';
+import { Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
 
 class SignUp extends Component {
   render () {
     return (
-      <div>
+      <div className="container">
         <h2>회원가입</h2>
+        <Form>
+          <FormGroup className="row">
+            <Label className="col-12 col-md-2" for="exampleEmail">Email</Label>
+            <Input className="col-12 col-md-10"  type="email" name="email" id="exampleEmail" placeholder="with a placeholder" />   
+          </FormGroup>
+          <FormGroup className="row">
+            <Label className="col-12 col-md-2" for="exampleEmail">Password</Label>
+            <Input className="col-12 col-md-10"  type="password" name="email" id="exampleEmail" placeholder="with a placeholder" />    
+          </FormGroup>
+          <FormGroup className="row">
+            <Label className="col-12 col-md-2" for="exampleEmail">Password 확인</Label>
+            <Input className="col-12 col-md-10"  type="password" name="email" id="exampleEmail" placeholder="with a placeholder" />  
+          </FormGroup>      
+          <FormGroup className="row">      
+            <Label className="col-12 col-md-2" for="exampleEmail">닉네임</Label>
+            <Input className="col-12 col-md-10"  type="email" name="email" id="exampleEmail" placeholder="with a placeholder" />         
+          </FormGroup>
+          <FormGroup className="row">      
+            <Label className="col-12 col-md-2" for="exampleEmail">프로필 사진</Label>
+            <Input className="col-12 col-md-10"  type="email" name="email" id="exampleEmail" placeholder="with a placeholder" />                                                      
+          </FormGroup>                    
+        </Form>
       </div>
     );
   }
