efc2b0b6abec4827841257d784e045e0a9ed53ab
kyoungah <kyoungah@kyoungah.com>
2017-11-12 01:24:29 +0900

    회원가입 폼 수정

--- src/container/user/SignUp.js
+++ src/container/user/SignUp.js
@@ -5,29 +5,38 @@ class SignUp extends Component {
   render () {
     return (
       <div className="container">
-        <h2>회원가입</h2>
-        <Form>
-          <FormGroup className="row">
-            <Label className="col-12 col-md-2" for="exampleEmail">Email</Label>
-            <Input className="col-12 col-md-10"  type="email" name="email" id="exampleEmail" placeholder="with a placeholder" />   
-          </FormGroup>
-          <FormGroup className="row">
-            <Label className="col-12 col-md-2" for="exampleEmail">Password</Label>
-            <Input className="col-12 col-md-10"  type="password" name="email" id="exampleEmail" placeholder="with a placeholder" />    
-          </FormGroup>
-          <FormGroup className="row">
-            <Label className="col-12 col-md-2" for="exampleEmail">Password 확인</Label>
-            <Input className="col-12 col-md-10"  type="password" name="email" id="exampleEmail" placeholder="with a placeholder" />  
-          </FormGroup>      
-          <FormGroup className="row">      
-            <Label className="col-12 col-md-2" for="exampleEmail">닉네임</Label>
-            <Input className="col-12 col-md-10"  type="email" name="email" id="exampleEmail" placeholder="with a placeholder" />         
-          </FormGroup>
-          <FormGroup className="row">      
-            <Label className="col-12 col-md-2" for="exampleEmail">프로필 사진</Label>
-            <Input className="col-12 col-md-10"  type="email" name="email" id="exampleEmail" placeholder="with a placeholder" />                                                      
-          </FormGroup>                    
-        </Form>
+        <div className="col-12 col-md-8 mx-auto">
+          <h2>회원가입</h2>
+          <Form>
+            <FormGroup className="row">
+              <Label className="col-12 col-md-3 col-form-label text-md-right pl-0 pr-4" for="exampleEmail">Email</Label>
+              <Input className="col-12 col-md-9"  type="email" name="email" id="exampleEmail" placeholder="Email을 입력해주세요." />   
+            </FormGroup>
+            <FormGroup className="row">
+              <Label className="col-12 col-md-3 col-form-label text-md-right pl-0 pr-4" for="exampleEmail">비밀번호</Label>
+              <Input className="col-12 col-md-9"  type="password" name="email" id="exampleEmail" placeholder="비밀번호를 입력해주세요." />    
+            </FormGroup>
+            <FormGroup className="row">
+              <Label className="col-12 col-md-3 col-form-label text-md-right pl-0 pr-4" for="exampleEmail">비밀번호 확인</Label>
+              <Input className="col-12 col-md-9"  type="password" name="email" id="exampleEmail" placeholder="비밀번호를 입력해주세요." />  
+            </FormGroup>      
+            <FormGroup className="row">      
+              <Label className="col-12 col-md-3 col-form-label text-md-right pl-0 pr-4" for="exampleEmail">닉네임</Label>
+              <Input className="col-12 col-md-9"  type="email" name="email" id="exampleEmail" placeholder="닉네임(최대 8글자)을 입력해주세요." />         
+            </FormGroup>
+            <FormGroup className="row">      
+              <Label className="col-12 col-md-3 col-form-label text-md-right pl-0 pr-4" for="exampleEmail">프로필 사진</Label>
+              <div className="col-12 col-md-9">
+                <div className="profile-input">
+                  <label htmlFor="imageFile">
+                    <i className="fa fa-file-image-o" aria-hidden="true" />
+                  </label>
+                  <input id="imageFile" type="file" accept="image/*" ref={input => this.fileEl = input} onChange={null}/>                  
+                </div>
+              </div>                                                       
+            </FormGroup>                    
+          </Form>        
+        </div>
       </div>
     );
   }
