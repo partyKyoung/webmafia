aa782f414457aaf2eeb13234be418e3103a619db
KyoungAh <kyoungah@kyoungah.com>
2017-12-01 00:06:06 +0900

    회원가입 폼 체크하는 함수 작성

--- src/redux/reducer/index.js
+++ src/redux/reducer/index.js
@@ -1,4 +1,4 @@
-import { createStore, combineReducers } from 'redux';
+import { combineReducers } from 'redux';
 import { reducer as formReducer } from 'redux-form';
 
 const reducer = combineReducers({
