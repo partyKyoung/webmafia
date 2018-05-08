ae17a946622a0e0b5f3c18e0a8ab99ab456953d5
Youkyoung Park <kyoungah@kyoungah.com>
2018-05-08 23:45:03 +0900

    header, footer 레이아웃 생성

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from 'react';
 
+import Footer from 'components/layout/Footer/Footer';
+
+import './App.scss';
+
 function App() {
   return (
     <div className="app">
       늑대온에어
+      <Footer />
     </div>
   );
 }
---
+++ src/components/layout/Footer/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './Footer.scss';
+
+function Footer() {
+  return (
+    <footer className="wo-footer">
+      <p>늑대온에어 by 경아</p>
+    </footer>
+  );
+}
+
+export default Footer;
