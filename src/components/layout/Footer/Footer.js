59f4a068d830400f2030bf02eeba9870624688c7
Youkyoung Park <kyoungah@kyoungah.com>
2018-05-08 23:54:02 +0900

    default css 수정

--- src/components/layout/Footer/Footer.js
+++ src/components/layout/Footer/Footer.js
@@ -4,7 +4,7 @@ import './Footer.scss';
 
 function Footer() {
   return (
-    <footer className="wo-footer">
+    <footer className="wo-footer pt-3 pb-3 text-center">
       <p>늑대온에어 by 경아</p>
     </footer>
   );
