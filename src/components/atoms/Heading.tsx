7e72154994706d5680556cdec7c5358acb56deab
partykyoung <kyoungah@kyoungah.com>
2021-04-08 23:28:48 +0900

    refac: Heading 컴포넌트 리팩토링

--- src/components/atoms/Heading.tsx
+++ src/components/atoms/Heading.tsx
@@ -1,74 +1,71 @@
-import React from 'react';
-
+import React, { forwardRef } from 'react';
 import styled, { css } from 'styled-components';
 
-interface HeadingProps {
+interface HeadingProps extends React.HtmlHTMLAttributes<HTMLHeadingElement> {
   children: React.ReactNode;
   level?: 1 | 2 | 3 | 4 | 5 | 6;
 }
 
-function Heading({
-  level = 1,
-  ...others
-}: HeadingProps): React.ReactElement {
+const Heading = forwardRef<HTMLHeadingElement, HeadingProps>(({
+  level,
+  ...props
+}: HeadingProps, ref) => {
   if (level === 6) {
-    return <H6 {...others} />
+    return <H6 ref={ref} {...props} />
   }
-  
+
   if (level === 5) {
-    return <H5 {...others} />
+    return <H5 ref={ref} {...props} />
   }
 
   if (level === 4) {
-    return <H4 {...others} />
+    return <H4 ref={ref} {...props} />
   }
 
   if (level === 3) {
-    return <H3 {...others} />
+    return <H3 ref={ref} {...props} />
   }
 
   if (level === 2) {
-    return <H2 {...others} />
+    return <H2 ref={ref} {...props} />
   }
 
-  return <H1 {...others} />
-};
+  return <H1 ref={ref} {...props} />
+});
 
 const styles = css`
-  font-weight: 600;
-  line-height: 1;
+  color: ${({theme}) => theme.colors.gray10};
+  font-weight: bold;
 `;
 
 const H1 = styled.h1`
-  ${styles}
-  font-size: 2rem;
+  ${styles};
+  font-size: 2.5rem;
 `;
 
 const H2 = styled.h2`
-  ${styles}
-  font-size: 1.625rem;
+  ${styles};
+  font-size: 2rem;
 `;
 
 const H3 = styled.h3`
-  ${styles}
-  font-size: 1.5rem;
+  ${styles};
+  font-size: 1.625rem;
 `;
 
 const H4 = styled.h4`
-  ${styles}
-  font-size: 1.25rem;
+  ${styles};
+  font-size: 1.5rem;
 `;
 
 const H5 = styled.h5`
-  ${styles}
-  font-size: 1.125rem;
+  ${styles};
+  font-size: 1.25rem;
 `;
 
 const H6 = styled.h6`
-  ${styles}
-  font-size: 1rem;
+  ${styles};
+  font-size: 1.125rem;
 `;
-
-
 
 export default Heading;
